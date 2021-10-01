7f8223b55075841f2fbae7dd4a92adbe10704aff
cdrh85 <cdrh85@gmail.com>
2021-09-30 21:08:14 -0700

    4-puts.c up file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  * main - print string with function puts
+ *   *
+ *    * Return: A string with a new line
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
