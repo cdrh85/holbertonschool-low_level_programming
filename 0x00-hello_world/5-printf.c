1a00469a9dcf4c84396e54bb6b9e065aeb4cf6fa
cdrh85 <cdrh85@gmail.com>
2021-09-30 22:11:53 -0700

    5-printf.ccorrection

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
-*main - print string with printf
-*  
-*Return: String with printf
-*/
+ *main - Print
+ *
+ *Description:prints exactly with proper grammar, but the outcome
+ *is a piece of art,, followed by a new line
+ *
+ *Return: returns 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
