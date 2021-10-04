85be45d5cecb5c6536b742f77888703d8454ddcb
cdrh85 <cdrh85@gmail.com>
2021-10-03 22:49:27 -0700

    8-correction base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,13 @@ int main(void)
 	int n, a;
 
 	for (n = 48; n < 58; n++)
-        {
+	{
 		putchar(n);
 	}
 	for (a = 'a'; a <= 'f'; a++)
 	{
 		putchar(a);
-	}								}
+	}
 	putchar('\n');
 	return (0);
 }
