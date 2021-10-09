0bc4d84ea5e5163d73dc6ce93f70b7784752e57d
cdrh85 <cdrh85@gmail.com>
2021-10-09 14:16:20 -0700

    correcciones

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,11 @@ lude "main.h"
  */
 void print_numbers(void)
 {
-int n;
+	int n;
 
-for (n = 0; n <= 9; n++)
-{
-putchar(n + '0');
-
-}
-putchar('\n');
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
 }
