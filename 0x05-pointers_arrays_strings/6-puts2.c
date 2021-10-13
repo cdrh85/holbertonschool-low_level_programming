695fbaf9ddca6b0b98537296ade4ddac689654ea
cdrh85 <cdrh85@gmail.com>
2021-10-12 17:23:47 -0700

    7 file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,17 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * puts2 - function that prints every other characteters
+ * @str: pointer to
  */
-int main(void)
+void puts2(char *str)
 {
-	char *str;
-
-	str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\0Holberton";
-	puts2(str);
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		if (*str == '\0')
+		break;
+		str++;
+	}
+	_putchar('\n');
 }
